898e4c8525f9e3baefa42fd8bf1c65b980ff722c
Renan de Alencar Queiroz <renanqueiroz96@gmail.com>
2024-07-14 15:14:28 -0400

    MS3

---
+++ SFT-MS3/project-starter/project-starter/SFT-MS3/SFT-MS3.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SFTMS3
+{
+	TEST_CLASS(SFTMS3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
