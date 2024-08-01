30fa238f852457f4685f80489777276cb011bb7f
Renan de Alencar Queiroz <renanqueiroz96@gmail.com>
2024-07-31 23:30:01 -0400

    11:29PM

--- ReactCourse/01-starting-project/src/App.jsx
+++ ReactCourse/01-starting-project/src/App.jsx
@@ -1,17 +1,45 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import PrintingMyName from "./components/PrintingMyName.jsx";
+import { MY_FAMILY } from "./data.js";
+import TabButton from "./components/TabButton.jsx";
+
+
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts //THIS IS THE LONGEST VERSION
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            {/*This is the shortest version*/}
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>States</TabButton>
+          </menu>
+        </section>
       </main>
+      <PrintingMyName {...MY_FAMILY[0]} />
+      <PrintingMyName {...MY_FAMILY[1]} />
+      <PrintingMyName {...MY_FAMILY[2]} />
+      <PrintingMyName {...MY_FAMILY[3]} />
     </div>
   );
 }
---
+++ ReactCourse/01-starting-project/src/components/PrintingMyName.jsx
@@ -0,0 +1,9 @@
+
+
+export default function PrintingMyName({ name, age }) {
+    return (
+      <p>
+        My name is: {name}, I'm {age} years-old
+      </p>
+    );
+  }
