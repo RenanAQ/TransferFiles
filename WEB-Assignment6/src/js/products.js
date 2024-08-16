5c8790c7ea5f05ed320a1bcb5f37fedb683722df
Renan de Alencar Queiroz <renanqueiroz96@gmail.com>
2024-08-16 18:38:52 -0400

    web

---
+++ WEB-Assignment6/src/js/products.js
@@ -0,0 +1,75 @@
+/**
+ * WEB222 – Assignment 05
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Renan de Alencar Queiroz
+ *      Student ID: 129280236
+ *      Date:       Jul 29, 2024
+ */
+window.products = [
+  {
+    id: "Cat01",
+    name: "Tennis Racquet",
+    title: "Babolat Tennis Racquet",
+    description:
+      "Experience ultimate power and control with the Babolat Adult Racquet, designed for players seeking performance and precision on the court.",
+    price: 27999,
+    discontinued: false,
+    categories: ["Cat01"],
+    imageUrl: "../src/images/TennisRacquet.png",
+  },
+
+  {
+    id: "Cat02",
+    name: "Tennis Balls",
+    title: "Wilson Tennis Balls",
+    description:
+      "Achieve consistent bounce and durability with Wilson Tennis Balls, perfect for both practice sessions and competitive matches.",
+    price: 499,
+    discontinued: false,
+    categories: ["Cat02"],
+    imageUrl: "../src/images/TennisBalls.jpeg",
+  },
+
+  {
+    id: "Cat03",
+    name: "Tennis Shoes",
+    title: "Yonex Tennis Shoes",
+    description:
+      "Step up your game with Yonex Tennis Shoes, offering exceptional comfort, stability, and traction for players of all levels.",
+    price: 17999,
+    discontinued: false,
+    categories: ["Cat03"],
+    imageUrl: "../src/images/TennisShoes.jpeg",
+  },
+
+  {
+    id: "Cat04",
+    name: "Tennis Bag",
+    title: "Wilson Tennis Bag",
+    description:
+      "Carry your gear in style with the Wilson Tennis Bag, featuring multiple compartments for easy organization and a sleek, modern design.",
+    price: 19999,
+    discontinued: false,
+    categories: ["Cat04"],
+    imageUrl: "../src/images/TennisBags.jpeg",
+  },
+
+  {
+    id: "Cat05",
+    name: "Tennis String",
+    title: "Luxilon Tennis String",
+    description:
+      "Enhance your racquet's performance with Luxilon Tennis String, known for its superior power, control, and durability.",
+    price: 3999,
+    discontinued: false,
+    categories: ["Cat05"],
+    imageUrl: "../src/images/TennisStrings.jpeg",
+  },
+];
