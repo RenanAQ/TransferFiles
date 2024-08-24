38952277ea96c5a5064dd2e38712c3f2b5ec5c73
Renan de Alencar Queiroz <renanqueiroz96@gmail.com>
2024-08-24 00:44:02 -0400

    tic-tac-toe-DONE

--- ReactCourse/02-tic-tac-toe-starting-project/src/components/Player.jsx
+++ ReactCourse/02-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,27 +1,38 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
-    const [playerName, setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
   //function to handle the button clicked
   function handleEditClick() {
-    setIsEditing((editing)=>!editing); //best practice
+    setIsEditing((editing) => !editing); //best practice
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
-  function handleChange(m_event){ //listener
+  function handleChange(m_event) {
+    //listener
     setPlayerName(m_event.target.value);
-  } 
+  }
   //variable to change the state
   let editablePlayerName = <span className="player-name">{playerName}</span>;
   let btnCaption = "Edit";
   //statement to change the variable if button clicked
   if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
     btnCaption = "Save";
   }
   //main function
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
