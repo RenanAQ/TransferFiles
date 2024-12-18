28e77c9748eff59df34e035c180f65639c2a70c5
Renan de Alencar Queiroz <renanqueiroz96@gmail.com>
2024-12-17 22:26:11 -0500

    assignment02-done

--- NodeCourse/assignment02/app.js
+++ NodeCourse/assignment02/app.js
@@ -2,17 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const m_adminRoutes = require("./routes/admin");
 const m_shopRoutes = require("./routes/shop")
+const path = require('path')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); //parsing body
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', m_adminRoutes);
 app.use(m_shopRoutes);
 
 //error handler
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not found!</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 
--- NodeCourse/assignment02/routes/admin.js
+++ NodeCourse/assignment02/routes/admin.js
@@ -1,14 +1,12 @@
 const express = require("express");
+const path = require("path")
+const rootDir = require('../utils/path')
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  console.log("In the middleware 1");
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="m_title"><button type="submit">Add m_Product</button></form>'
-  );
-  // next(); //this allows the request to continue to the next middleware if we do not have a response
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
