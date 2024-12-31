1f0bdadc645c8c6ec4ecf207e61dbd7431ea8494
Renan <renanqueiroz96@gmail.com>
2024-12-31 13:24:23 -0500

    forms

---
+++ NodeCourse/trainning01/app.js
@@ -0,0 +1,22 @@
+//header for express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//header for ejs
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+
+//middleware form
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("body", { m_pageTitle: "Area Calc", value:"" });
+});
+
+//resource folder
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Listening @ ${port}`);
+});
---
+++ NodeCourse/trainning01/controller/form.js
@@ -0,0 +1,4 @@
+exports.formControl = (e)=>{
+    const selectedOption = e.target.value;
+    window.location.href = ``
+}
